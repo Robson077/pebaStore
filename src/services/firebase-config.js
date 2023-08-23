defa9b9b6fe4c817c90bc60e0b294cf347e68354
Robson077 <minhamae830@gmail.com>
2023-08-23 00:01:01 -0300

    feat: mudando nome para um mais profissional

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
     apiKey: "AIzaSyAtXvUSJqkI1v4cmupUJC6-JmnhyEHults",
@@ -15,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
