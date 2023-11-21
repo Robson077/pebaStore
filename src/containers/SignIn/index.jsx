2ed0014897aba177ac9beda83b139e1a5de5dbfa
robson077 <minhamae830@gmail.com>
2023-11-20 23:18:11 -0300

    feat: removendo firebase

--- src/containers/SignIn/index.jsx
+++ src/containers/SignIn/index.jsx
@@ -2,70 +2,18 @@ import { Container, Title, Form, LabelEmail, InputEmail, LabelSenha, InputSenha,
 
 import { BsFacebook, BsGoogle, BsArrowLeft, BsArrowRight } from "react-icons/bs"
 
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
-import { auth, provider, providerFace } from "../../services/firebase-config"
-import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth"
 
 import { useState } from "react"
 
 import fotoBack from "../../assets/bela-praia.jpg"
 
 function SignIn() {
-    const navigate = useNavigate()
 
     const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
 
-    const handleSignIn = async() => {
-        try {
-            if(email.length && senha.length) {
-                const userCredetial = await signInWithEmailAndPassword(auth, email, senha)
-                const user = userCredetial.user
-                
-                console.log(user)
-                console.log("logou")
-                navigate("/Content")
-            } else {
-                alert("Digite email e senha")
-            }
-        } catch (error) {
-            alert("Email ou Senha estão incorretos")
-            console.error("teste", error)
-        }
-    }
-
-    const handleSignWithGoogle = async() => {
-        try {
-            const result = await signInWithPopup(auth, provider)
-
-            const credential = GoogleAuthProvider.credentialFromResult(result)
-            const token = credential.accessToken
-
-            const user = result.user
-            console.log(token)
-            console.log(user)
-            navigate("/Content")
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    const handleSignWithFacebook = async() => {
-        try {
-            const result = await signInWithPopup(auth, providerFace)
-
-            const credential = FacebookAuthProvider.credentialFromResult(result)
-            const token = credential.accessToken
-
-            const user = result.user
-            console.log(token)
-            console.log(user)
-            navigate("/Content")
-        } catch (error) {
-            console.error(error)
-        }
-    }
 
     return (
         <ContainerFoto img={fotoBack}>
@@ -99,13 +47,13 @@ function SignIn() {
                 </Form>
 
                 <Redes>
-                    <BsFacebook size={60} onClick={handleSignWithFacebook} />
-                    <BsGoogle size={60} onClick={handleSignWithGoogle}/>
+                    <BsFacebook size={60}/>
+                    <BsGoogle size={60}/>
                 </Redes>
 
                 <Direcionamentos>
                     <Link to="/SignUp"><SignUp>Sign Up</SignUp></Link>
-                    <Button onClick={handleSignIn}><BsArrowRight size={35} color="#fff"/></Button>
+                    <Button><BsArrowRight size={35} color="#fff"/></Button>
                 </Direcionamentos>
             
             </Container>
--- src/containers/SignUp/index.jsx
+++ src/containers/SignUp/index.jsx
@@ -2,76 +2,17 @@ import { Container, Title, Form, LabelEmail, InputEmail, LabelSenha, InputSenha,
 
 import { BsFacebook, BsGoogle, BsArrowLeft, BsArrowRight } from "react-icons/bs"
 
-import { Link, useNavigate } from "react-router-dom"
-
-
-import { auth, provider, providerFace } from "../../services/firebase-config"
-import { createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider} from "firebase/auth"
+import { Link } from "react-router-dom"
 
 import { useState } from "react"
 
 import fotoBack from "../../assets/bela-praia.jpg"
 
 function SignUp() {
-    const navigate = useNavigate()
-
-    const user = auth.currentUser
-    console.log(user)
 
     const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
     
-
-    const handleCreateUser = async() => {
-        try {
-            if(email.length && senha.length > 0) {
-                await createUserWithEmailAndPassword(auth, email, senha)
-                // history.push("/SignIn")
-                console.log("Usuario criado")
-                navigate("/Content")
-                // redirect("/Content")
-            } else {
-                alert("Digite seu email e senha")
-            }
-            
-        } catch (error) {
-            console.error("Erro de login:", error)
-        }
-    }
-
-    const handleSignWithGoogle = async() => {
-        try {
-            const result = await signInWithPopup(auth, provider)
-
-            const credential = GoogleAuthProvider.credentialFromResult(result)
-            const token = credential.accessToken
-
-            const user = result.user
-            console.log(token)
-            console.log(user)
-            navigate("/Content")
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    const handleSignWithFacebook = async() => {
-        try {
-            const result = await signInWithPopup(auth, providerFace)
-
-            const credential = FacebookAuthProvider.credentialFromResult(result)
-            const token = credential.accessToken
-
-            const user = result.user
-            console.log(token)
-            console.log(user)
-            navigate("/Content")
-        } catch (error) {
-            console.error(error)
-        }
-    }
-    
-
     return (
         <ContainerFoto img={fotoBack}>
             <Container>
@@ -101,12 +42,12 @@ function SignUp() {
             </Form>
 
             <Redes>
-                <BsFacebook size={60} onClick={handleSignWithFacebook} />
-                <BsGoogle size={60} onClick={handleSignWithGoogle} />
+                <BsFacebook size={60} />
+                <BsGoogle size={60} />
             </Redes>
 
             <Direcionamentos>
-                <Button onClick={handleCreateUser}><BsArrowRight size={35} color="#fff"/></Button>
+                <Button><BsArrowRight size={35} color="#fff"/></Button>
                 {/* <Button onClick={handleSignOut}>LogOut</Button> */}
             </Direcionamentos>
         </Container>
